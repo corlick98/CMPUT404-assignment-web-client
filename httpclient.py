5bb1fc666c427ca2adc372c6dd06f7a5d09602cb
Christopher Orlick <corlick@ualberta.ca>
2022-10-11 20:40:11 -0600

    probably finished

--- httpclient.py
+++ httpclient.py
@@ -40,14 +40,40 @@ class HTTPClient(object):
         self.socket.connect((host, port))
         return None
 
+    def extract_url_parts(self, url):
+        """
+        Extract the host, port, and path from the url
+        """
+        parsed = urllib.parse.urlparse(url)
+        port = parsed.port if parsed.port else 80
+        path = parsed.path if parsed.path else "/"
+        host = parsed.hostname
+        if host is None:
+            if path.find("/") == -1:
+                path += "/"
+            host = path[0:path.find("/")]
+            path = path[path.find("/"):]
+        return host, port, path
+
     def get_code(self, data):
-        return None
+        # split at spaces and extract code
+        return int(data.split()[1])
 
     def get_headers(self,data):
-        return None
+        # remove body
+        headers = data.split("\r\n\r\n")[0]
+        # remove status line
+        headers = headers.splitlines()[1:]
+        headers_dict = {}
+        for line in headers:
+            name, value = line.split(":")
+            headers_dict[name] = value
+        return headers_dict
 
     def get_body(self, data):
-        return None
+        # remove status line and headers
+        body = data.split("\r\n\r\n")[1:]
+        return "\r\n\r\n".join(body)
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,13 +94,42 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        host, port, path = self.extract_url_parts(url)
+        self.connect(host, port)
+        # create status line and headers
+        body = "GET {path} HTTP/1.1\r\n".format(path=path)
+        body += "Host: {host}:{port}\r\n".format(host=host, port=port)
+        body += "Connection: close\r\n"
+        body += "Accept: */*\r\n\r\n"
+
+        # send the request
+        self.sendall(body)
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        host, port, path = self.extract_url_parts(url)
+        self.connect(host, port)
+        # create status line and headers
+        body = "POST {path} HTTP/1.1\r\n".format(path=path)
+        body += "Host: {host}:{port}\r\n".format(host=host, port=port)
+        body += "Connection: close\r\n"
+        form_body = ""
+        if args != None:
+            form_body = urllib.parse.urlencode(args)
+            body += "Content-Type: application/x-www-form-urlencoded\r\n"
+        body += "Content-Length: {}\r\n\r\n".format(len(form_body))
+        body += form_body
+
+        # send the request
+        self.sendall(body)
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        self.close()
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
